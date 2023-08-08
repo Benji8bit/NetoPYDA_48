74767451fcf5ea3e27fcf5c742e6664096015c13
Benji8bit <coin8bit@hotmail.com>
2023-08-09 00:02:42 +0300

    Added task 4

--- hw1/hw1_v1.py
+++ hw1/hw1_v1.py
@@ -24,7 +24,6 @@ if year % 4 == 0:
 else:
     print("Обычный год")
 
-
 # Task 3
 
 print("Введите день и месяц своего рождения.")
@@ -32,62 +31,62 @@ print("Введите день и месяц своего рождения.")
 day_of_birth = int(input("Введите день: "))
 month_of_birth = input("Введите месяц: ")
 
-if month_of_birth == "Январь":
+if month_of_birth.capitalize() == "Январь":
     if day_of_birth < 20:
         print("Ваш знак зодиака: Козерог")
     else:
         print("Ваш знак зодиака: Водолей")
-elif month_of_birth == "Февраль":
+elif month_of_birth.capitalize() == "Февраль":
     if day_of_birth < 19:
         print("Ваш знак зодиака: Водолей")
     else:
         print("Ваш знак зодиака: Рыбы")
-elif month_of_birth == "Март":
+elif month_of_birth.capitalize() == "Март":
     if day_of_birth < 21:
         print("Ваш знак зодиака: Рыбы")
     else:
         print("Ваш знак зодиака: Овен")
-elif month_of_birth == "Апрель":
+elif month_of_birth.capitalize() == "Апрель":
     if day_of_birth < 20:
         print("Ваш знак зодиака: Овен")
     else:
         print("Ваш знак зодиака: Телец")
-elif month_of_birth == "Май":
+elif month_of_birth.capitalize() == "Май":
     if day_of_birth < 21:
         print("Ваш знак зодиака: Телец")
     else:
         print("Ваш знак зодиака: Близнецы")
-elif month_of_birth == "Июнь":
+elif month_of_birth.capitalize() == "Июнь":
     if day_of_birth < 21:
         print("Ваш знак зодиака: Близнецы")
     else:
         print("Ваш знак зодиака: Рак")
-elif month_of_birth == "Июль":
+elif month_of_birth.capitalize() == "Июль":
     if day_of_birth < 23:
         print("Ваш знак зодиака: Рак")
     else:
         print("Ваш знак зодиака: Лев")
-elif month_of_birth == "Август":
+elif month_of_birth.capitalize() == "Август":
     if day_of_birth < 23:
         print("Ваш знак зодиака: Лев")
     else:
         print("Ваш знак зодиака: Дева")
-elif month_of_birth == "Сентябрь":
+elif month_of_birth.capitalize() == "Сентябрь":
     if day_of_birth < 23:
         print("Ваш знак зодиака: Дева")
     else:
         print("Ваш знак зодиака: Весы")
-elif month_of_birth == "Октябрь":
+elif month_of_birth.capitalize() == "Октябрь":
     if day_of_birth < 23:
         print("Ваш знак зодиака: Весы")
     else:
         print("Ваш знак зодиака: Скорпион")
-elif month_of_birth == "Ноябрь":
+elif month_of_birth.capitalize() == "Ноябрь":
     if day_of_birth < 22:
         print("Ваш знак зодиака: Скорпион")
     else:
         print("Ваш знак зодиака: Стрелец")
-elif month_of_birth == "Декабрь":
+elif month_of_birth.capitalize() == "Декабрь":
     if day_of_birth < 22:
         print("Ваш знак зодиака: Стрелец")
     else:
@@ -97,7 +96,17 @@ else:
 
 # Task 4
 
-width = 15
-length = 55
-height = 15
+width = 45
+length = 205
+height = 45
 
+if width <= 15 and length <= 15 and height <= 15:
+    print("Коробка №1")
+elif width > 200 or length > 200 or height > 200:
+    print("Упаковка для лыж")
+elif (15 < width < 50) or (15 < length < 50) or (15 < height < 50):
+    print("Коробка №2")
+else:
+    print("Коробка №3")
+
+# Task 5
