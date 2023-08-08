0ece11f3fb9e9747cbc6f92a761614c12346e2d0
Benji8bit <coin8bit@hotmail.com>
2023-08-08 23:22:33 +0300

    Added task 2

--- hw1/hw1_v1.py
+++ hw1/hw1_v1.py
@@ -3,18 +3,27 @@
 phrase_1 = 'Насколько проще было бы писать программы, если бы не заказчики'
 phrase_2 = '640Кб должно хватить для любых задач. Билл Гейтс (по легенде)'
 
-if (len(phrase_1) > len(phrase_2)):
+if len(phrase_1) > len(phrase_2):
     print("Фраза 1 длиннее фразы 2")
-elif (len(phrase_2) > len(phrase_1)):
+elif len(phrase_2) > len(phrase_1):
     print("Фраза 2 длиннее фразы 1")
 else:
     print("Фразы равной длины")
 
 # Task 2
 
-year = 2020
+year = 2100
 
-if ((year%4)==0) and ((year%100)==0) and ((year%400)==0):
-    print("Високосный год")
+if year % 4 == 0:
+    if ((year % 100) == 0) and ((year % 400) == 0):
+        print("Високосный год")
+    elif year % 100 == 0:
+        print("Обычный год")
+    else:
+        print("Високосный год")
 else:
     print("Обычный год")
+
+
+ Task 3
+
