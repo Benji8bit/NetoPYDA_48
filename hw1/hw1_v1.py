ad51a285fd6960411286b69d573bb6fd13744bfc
Benji8bit <coin8bit@hotmail.com>
2023-08-08 23:50:09 +0300

    Added task 3

--- hw1/hw1_v1.py
+++ hw1/hw1_v1.py
@@ -25,5 +25,79 @@ else:
     print("Обычный год")
 
 
- Task 3
+# Task 3
+
+print("Введите день и месяц своего рождения.")
+
+day_of_birth = int(input("Введите день: "))
+month_of_birth = input("Введите месяц: ")
+
+if month_of_birth == "Январь":
+    if day_of_birth < 20:
+        print("Ваш знак зодиака: Козерог")
+    else:
+        print("Ваш знак зодиака: Водолей")
+elif month_of_birth == "Февраль":
+    if day_of_birth < 19:
+        print("Ваш знак зодиака: Водолей")
+    else:
+        print("Ваш знак зодиака: Рыбы")
+elif month_of_birth == "Март":
+    if day_of_birth < 21:
+        print("Ваш знак зодиака: Рыбы")
+    else:
+        print("Ваш знак зодиака: Овен")
+elif month_of_birth == "Апрель":
+    if day_of_birth < 20:
+        print("Ваш знак зодиака: Овен")
+    else:
+        print("Ваш знак зодиака: Телец")
+elif month_of_birth == "Май":
+    if day_of_birth < 21:
+        print("Ваш знак зодиака: Телец")
+    else:
+        print("Ваш знак зодиака: Близнецы")
+elif month_of_birth == "Июнь":
+    if day_of_birth < 21:
+        print("Ваш знак зодиака: Близнецы")
+    else:
+        print("Ваш знак зодиака: Рак")
+elif month_of_birth == "Июль":
+    if day_of_birth < 23:
+        print("Ваш знак зодиака: Рак")
+    else:
+        print("Ваш знак зодиака: Лев")
+elif month_of_birth == "Август":
+    if day_of_birth < 23:
+        print("Ваш знак зодиака: Лев")
+    else:
+        print("Ваш знак зодиака: Дева")
+elif month_of_birth == "Сентябрь":
+    if day_of_birth < 23:
+        print("Ваш знак зодиака: Дева")
+    else:
+        print("Ваш знак зодиака: Весы")
+elif month_of_birth == "Октябрь":
+    if day_of_birth < 23:
+        print("Ваш знак зодиака: Весы")
+    else:
+        print("Ваш знак зодиака: Скорпион")
+elif month_of_birth == "Ноябрь":
+    if day_of_birth < 22:
+        print("Ваш знак зодиака: Скорпион")
+    else:
+        print("Ваш знак зодиака: Стрелец")
+elif month_of_birth == "Декабрь":
+    if day_of_birth < 22:
+        print("Ваш знак зодиака: Стрелец")
+    else:
+        print("Ваш знак зодиака: Козерог")
+else:
+    print("Уточните ввод.")
+
+# Task 4
+
+width = 15
+length = 55
+height = 15
 
