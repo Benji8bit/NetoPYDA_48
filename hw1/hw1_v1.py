06d8511e36f0c18e11a818b4a4c35bb1e7083b16
Benji8bit <coin8bit@hotmail.com>
2023-08-10 00:36:48 +0300

    Added task 6

--- hw1/hw1_v1.py
+++ hw1/hw1_v1.py
@@ -26,73 +26,104 @@ else:
 
 # Task 3
 
+import sys
+
 print("Введите день и месяц своего рождения.")
 
-day_of_birth = int(input("Введите день: "))
+try:
+    day_of_birth = int(input("Введите день: "))
+except ValueError:
+    print("Эх, нужно было вводить число...")
+    sys.exit()
 month_of_birth = input("Введите месяц: ")
 
 if month_of_birth.capitalize() == "Январь":
-    if day_of_birth < 20:
+    if 0 < day_of_birth < 20:
         print("Ваш знак зодиака: Козерог")
-    else:
+    elif 19 < day_of_birth < 32:
         print("Ваш знак зодиака: Водолей")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Февраль":
-    if day_of_birth < 19:
+    if 0 < day_of_birth < 19:
         print("Ваш знак зодиака: Водолей")
-    else:
+    elif 18 < day_of_birth < 30:
         print("Ваш знак зодиака: Рыбы")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Март":
-    if day_of_birth < 21:
+    if 0 < day_of_birth < 21:
         print("Ваш знак зодиака: Рыбы")
-    else:
+    elif 20 < day_of_birth < 32:
         print("Ваш знак зодиака: Овен")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Апрель":
-    if day_of_birth < 20:
+    if 0 < day_of_birth < 20:
         print("Ваш знак зодиака: Овен")
-    else:
+    elif 19 < day_of_birth < 31:
         print("Ваш знак зодиака: Телец")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Май":
-    if day_of_birth < 21:
+    if 0 < day_of_birth < 21:
         print("Ваш знак зодиака: Телец")
-    else:
+    elif 20 < day_of_birth < 32:
         print("Ваш знак зодиака: Близнецы")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Июнь":
-    if day_of_birth < 21:
+    if 0 < day_of_birth < 21:
         print("Ваш знак зодиака: Близнецы")
-    else:
+    elif 20 < day_of_birth < 31:
         print("Ваш знак зодиака: Рак")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Июль":
-    if day_of_birth < 23:
+    if 0 < day_of_birth < 23:
         print("Ваш знак зодиака: Рак")
-    else:
+    elif 22 < day_of_birth < 32:
         print("Ваш знак зодиака: Лев")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Август":
-    if day_of_birth < 23:
+    if 0 < day_of_birth < 23:
         print("Ваш знак зодиака: Лев")
-    else:
+    elif 22 < day_of_birth < 32:
         print("Ваш знак зодиака: Дева")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Сентябрь":
-    if day_of_birth < 23:
+    if 0 < day_of_birth < 23:
         print("Ваш знак зодиака: Дева")
-    else:
+    elif 22 < day_of_birth < 31:
         print("Ваш знак зодиака: Весы")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Октябрь":
-    if day_of_birth < 23:
+    if 0 < day_of_birth < 23:
         print("Ваш знак зодиака: Весы")
-    else:
+    elif 22 < day_of_birth < 32:
         print("Ваш знак зодиака: Скорпион")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Ноябрь":
-    if day_of_birth < 22:
+    if 0 < day_of_birth < 22:
         print("Ваш знак зодиака: Скорпион")
-    else:
+    elif 21 < day_of_birth < 31:
         print("Ваш знак зодиака: Стрелец")
+    else:
+        print("Уточните ввод дня.")
 elif month_of_birth.capitalize() == "Декабрь":
-    if day_of_birth < 22:
+    if 0 < day_of_birth < 22:
         print("Ваш знак зодиака: Стрелец")
-    else:
+    elif 21 < day_of_birth < 32:
         print("Ваш знак зодиака: Козерог")
+    else:
+        print("Уточните ввод дня.")
 else:
-    print("Уточните ввод.")
+    print("Уточните ввод месяца.")
+
 
 # Task 4
 
@@ -113,8 +144,38 @@ else:
 
 number = 123456
 
-if (number//100000 + number%100000//10000 + number%10000//1000) == (number%1000//100 + number%100//10 + number%10):
+if (number // 100000 + number % 100000 // 10000 + number % 10000 // 1000) == (
+        number % 1000 // 100 + number % 100 // 10 + number % 10):
     print("Счастливый билет")
 else:
     print("Несчастливый билет")
 
+# Task 6
+
+import math
+
+figure_type = input("Введите тип геометрической фигуры: ")
+
+if figure_type.capitalize() == "Круг":
+    circle_radius = int(input("Введите длину радиуса круга: "))
+    circle_square = math.pi * circle_radius ** 2
+    print("Площадь круга: " + str(circle_square))
+
+elif figure_type.capitalize() == "Треугольник":
+    triangle_side_A = int(input("Введите длину стороны А: "))
+    triangle_side_B = int(input("Введите длину стороны B: "))
+    triangle_side_C = int(input("Введите длину стороны C: "))
+    half_perimeter = (triangle_side_A + triangle_side_B + triangle_side_C) / 2
+    triangle_square = math.sqrt(
+        half_perimeter * (half_perimeter - triangle_side_A) * (half_perimeter - triangle_side_B) * (
+                half_perimeter - triangle_side_C))
+    print("Площадь треугольника: " + str(triangle_square))
+
+elif figure_type.capitalize() == "Прямоугольник":
+    rectangle_side_A = int(input("Введите длину стороны А: "))
+    rectangle_side_B = int(input("Введите длину стороны B: "))
+    rectangle_square = rectangle_side_A * rectangle_side_B
+    print("Площадь прямоугольника: " + str(rectangle_square))
+
+else:
+    print("Какой блин гексаоктаэдр? Введите круг, треугольник или прямоугольник.")
